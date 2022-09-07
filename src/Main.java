9c2eea1f39172a107de21c587e6c52ce7da16d28
Pryanikspb <konstantinprivator@gmail.com>
2022-09-07 18:11:56 +0300

    Homework complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author fedorDostoevsky = new Author("Fedor", "Dostoevsky");
+        Book crimeAndPunishment = new Book("Crime and punishment", 1970, fedorDostoevsky);
+        crimeAndPunishment.setPublishDate(1900);
+        System.out.println(crimeAndPunishment);
+
+        Author mikhailBulgakov = new Author("Mikhail", "Bulgakov");
+        Book dogsHeart = new Book("Dogs Heart", 1920, mikhailBulgakov);
+        System.out.println(dogsHeart);
+
     }
 }
